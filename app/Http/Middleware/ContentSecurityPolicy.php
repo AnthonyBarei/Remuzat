d92abd4995866c7c1fca775c86b452840a099090
anthony <anthony.barei@outlook.fr>
2026-02-12 22:54:27 +0100

    Add Content Security Policy middleware and integrate CSP nonce in app view

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Vite;
+
+class ContentSecurityPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $nonce = Vite::useCspNonce();
+
+        $response = $next($request);
+
+        if (method_exists($response, 'header')) {
+            $response->header('Content-Security-Policy', $this->buildPolicy($nonce));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Build the Content Security Policy string.
+     */
+    protected function buildPolicy(string $nonce): string
+    {
+        $directives = [
+            "default-src" => "'self'",
+            "script-src"  => "'self' 'nonce-{$nonce}'",
+            "style-src"   => "'self' 'unsafe-inline' https://fonts.googleapis.com",
+            "font-src"    => "'self' https://fonts.gstatic.com",
+            "img-src"     => "'self' data: blob:",
+            "connect-src" => "'self'" . (app()->environment('local') ? " ws://localhost:* http://localhost:*" : ""),
+        ];
+
+        return collect($directives)
+            ->map(fn ($value, $key) => "{$key} {$value}")
+            ->implode('; ');
+    }
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,7 @@
         {{-- <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> --}}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
 
-        <style>
+        <style nonce="{{ Vite::cspNonce() }}">
             body {
                 font-family: 'Nunito', sans-serif;
             }
